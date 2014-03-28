7b1e574fefc54a857e9f835aafd7fd753799f159
Stefan Schoeb <opendix@gmail.com>
2014-03-28 14:31:50 +0100

    Initial

---
+++ CommandsTest/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PortableCommandTest;
+
+namespace CommandsTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var class1 = new Class1();
+        }
+    }
+}
---
+++ PortableCommandTest/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Input;
+using Cirrious.MvvmCross.ViewModels;
+
+namespace PortableCommandTest
+{
+    public class Class1
+    {
+
+        public ICommand Command { get; set; }
+
+        public Class1()
+        {
+
+            Command = new MvxCommand(Execute, CanExecute);
+        }
+
+        private bool CanExecute()
+        {
+            return true;
+        }
+
+        private void Execute()
+        {
+        }
+    }
+}
